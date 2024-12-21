18be7793b839f75e5fcd14b739c9e83009fee507
LittleMangoez <matthaydensmith9@gmail.com>
2024-12-21 15:48:39 -0500

    Created user initializer, added overview text (to be edited)

--- trailhead/ContentView.swift
+++ trailhead/ContentView.swift
@@ -10,51 +10,54 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @Query private var user: [User]
+    @State var stableUser: User?
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack(alignment: .leading) {
+            Text(Date().formatted(date: .abbreviated, time: .omitted).description)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            // Top overview text
+            HStack {
+                let name = stableUser?.name ?? "User"
+                let taskCount = stableUser?.tasks.count ?? 0 <= 0 ? "no" : stableUser?.tasks.count.description
+                let taskIcon = Image(systemName: "checklist")
+                let habitCount = stableUser?.habits.count ?? 0 <= 0 ? "no" : stableUser?.habits.count.description
+                let habitIcon = Image(systemName: "point.forward.to.point.capsulepath.fill")
+                
+                Text("Hello, \(name)! \(Image(systemName: "heart.fill")), you have \(taskIcon) \(taskCount!) tasks and \(habitIcon) \(habitCount!) habits to complete today.")
+                    .padding(.top, 3)
             }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            
+            
+            // Task List View
+            
+            Spacer()
+            
+            // Drawer
+            
+            
+        }
+        .padding(.horizontal)
+        .onAppear {
+            initializeUser()
         }
     }
-
-    private func addItem() {
+    
+    private func initializeUser() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+            if user.isEmpty {
+                print("Creating new Jane Doe...")
+                let newUserPreferences = Preference(isNotificationsEnabled: true)
+                let newUser = User(joinDate: Date(), name: "Jane Doe", tasks: [], habits: [], prefs: newUserPreferences, classes: [])
+                modelContext.insert(newUser)
+                self.stableUser = newUser
+            } else {
+                print("Pulling stored user...")
+                self.stableUser = user.first
+                
             }
         }
     }
@@ -62,5 +65,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: User.self, inMemory: true)
 }
